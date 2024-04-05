373cb99cbcdab57053de85321d967d6096b57d4b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-05 21:43:35 +0200

    CarModel - klasa bazowa

---
+++ src/main/java/com/example/carsapi/model/CarModel.java
@@ -0,0 +1,22 @@
+package com.example.carsapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class CarModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private String color;
+}
