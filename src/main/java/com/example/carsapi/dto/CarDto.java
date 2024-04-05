27b588c1ba6bbc1e5504e81f5417a8f6abf7c03b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-05 21:43:07 +0200

    CarDto - klasa

---
+++ src/main/java/com/example/carsapi/dto/CarDto.java
@@ -0,0 +1,12 @@
+package com.example.carsapi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CarDto {
+    private String brand;
+    private String model;
+    private String color;
+}
