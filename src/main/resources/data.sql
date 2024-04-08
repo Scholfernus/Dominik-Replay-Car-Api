f60844f4bcccb06822e617cf8cf5769746017ecc
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-04-08 22:03:26 +0200

    Properties i data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO car_model (brand, model, color) VALUES
+                                                    ('Ford', 'Escort', 'red'),
+                                                    ('Mazda', 'Comfort', 'yellow'),
+                                                    ('Opel', 'Astra', 'green'),
+                                                    ('Suzuki', 'X-Trail', 'white');
